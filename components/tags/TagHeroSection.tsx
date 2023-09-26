44f44be582e4b5a72df2492cfab89d3d7151e2dc
이나령 <thisisme20@daum.net>
2023-09-26 17:58:17 +0900

    :sparkles: Feat: tag별 모아보기 페이지 완성

--- components/tags/TagHeroSection.tsx
+++ components/tags/TagHeroSection.tsx
@@ -10,7 +10,7 @@ const TagHeroSection = ({ subtitle = "Tag collection", title = "All Tags"}:TagHe
     <section>
       <div className="w-4/5 mx-auto max-w-5xl py-16 flex flex-col gap-4">
         <p className="font-medium text-gray-700 text-2xl">{subtitle}</p>
-        <h2 className="font-bold text-6xl">{title}</h2>
+        <h2 className="font-bold text-6xl">#{title}</h2>
       </div>
     </section>
   )
--- pages/tag/[tagName].tsx
+++ pages/tag/[tagName].tsx
@@ -1,4 +1,5 @@
 import { getDatabaseItems } from "@/cms/notionClient";
+import CardSection from "@/components/intro/CardSection";
 import TagHeroSection from "@/components/tags/TagHeroSection";
 import { getAllTags } from "@/utils/getAllTags";
 import { ParsedDatabaseItemType, parseDatabaseItems } from "@/utils/parseDatabaseItem";
@@ -18,6 +19,7 @@ const TagPage = ({ databaseItems, tagName }: TagPageProps) => {
   return (
     <>
       <TagHeroSection title={tagName} />
+      <CardSection cardItems={databaseItems} />
     </>
   );
 };
