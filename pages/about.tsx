70e55622ca6907f1f70c0f74d2438966fbb69767
이나령 <thisisme20@daum.net>
2023-10-04 02:05:49 +0900

    :lipstick: Style: about 페이지의 title, subtitle 추가

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,15 +2,21 @@ import { getPageContent } from "@/cms/notionClient";
 import NotionPageRenderer from "@/components/notion/NotionPageRenderer";
 import { GetStaticProps } from "next";
 import { ExtendedRecordMap } from "notion-types";
-import React from 'react'
+import React from "react";
 
 interface AboutPageProps {
   recordMap: ExtendedRecordMap;
 }
 const AboutPage = ({ recordMap }: AboutPageProps) => {
-  return(
+  return (
     <>
-      <NotionPageRenderer recordMap={recordMap} />
+      <section>
+        <div className="w-4/5 mx-auto max-w-5xl py-16 flex flex-col gap-4">
+          <p className="font-medium text-gray-700 text-2xl">Hello</p>
+          <h2 className="font-bold text-6xl">About Me</h2>
+          <NotionPageRenderer recordMap={recordMap} />
+        </div>
+      </section>
     </>
   );
 };
@@ -25,6 +31,6 @@ export const getStaticProps: GetStaticProps<AboutPageProps> = async () => {
   return {
     props: {
       recordMap,
-    }
-  }
-}
+    },
+  };
+};
